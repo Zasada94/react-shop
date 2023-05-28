48a6d6e68ce4ecdb15abc0f8f5031f6be8059148
Zasada94 <pawelzasada2@wp.pl>
2023-05-28 14:26:18 +0200

    navbar development

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,35 @@ import styled from "styled-components";
 
 const Container = styled.div`
 	height: 60px;
-	background-color: black;
+	background-color: gray;
+`;
+
+const Wrapper = styled.div`
+	padding: 10px 20px;
+	display: flex;
+	justify-content: space-between;
+`;
+
+const Left = styled.div`
+	flex: 1;
+`;
+const Center = styled.div`
+	flex: 1;
+`;
+const Right = styled.div`
+	flex: 1;
 `;
 
 const Navbar = () => {
-	return <Container>Navbar</Container>;
+	return (
+		<Container>
+			<Wrapper>
+				<Left>Left</Left>
+				<Center>Center</Center>
+				<Right>Right</Right>
+			</Wrapper>
+		</Container>
+	);
 };
 
 export default Navbar;
