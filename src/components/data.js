fcf3ed91fa97dbe7fee6286f5ac47d515eb0708a
Zasada94 <pawelzasada2@wp.pl>
2023-06-01 15:15:17 +0200

    desktop categories finished

--- src/components/data.js
+++ src/components/data.js
@@ -2,12 +2,16 @@ import image1 from "../images/banner1.png";
 import image2 from "../images/banner2.png";
 import image3 from "../images/banner3.png";
 
+import catImage1 from "../images/catImage1.jpg";
+import catImage2 from "../images/catImage2.jpg";
+import catImage3 from "../images/catimage3.jpg";
+
 export const sliderItems = [
 	{
 		id: 1,
 		img: image1,
 		title: "SUMMER SALE",
-		desc: "ON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
 		bg: "f5fafd",
 	},
 	{
@@ -23,5 +27,23 @@ export const sliderItems = [
 		title: "ACCESSORIES SALE",
 		desc: "UNLEASH YOUR STYLE WITH CONFIDENCE! ENJOY 10% OFF ACCESSORIES!",
 		bg: "fbf0f4",
+	},
+];
+
+export const categories = [
+	{
+		id: 1,
+		img: catImage1,
+		title: "T-SHIRT STYLE!",
+	},
+	{
+		id: 2,
+		img: catImage2,
+		title: "OVERWEAR LOVE",
+	},
+	{
+		id: 3,
+		img: catImage3,
+		title: "ACCESSORIES",
 	},
 ];
