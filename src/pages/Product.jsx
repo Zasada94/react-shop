24d067d5d70d98ee769c5229d10e382d52ea635b
Zasada94 <pawelzasada2@wp.pl>
2023-06-10 14:05:42 +0200

    single product despktop page finished

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,6 +4,7 @@ import Announcement from "../components/Announcement";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 import popularProduct1 from "../images/popularProduct1.png";
+import { Add, Remove } from "@mui/icons-material";
 
 const Container = styled.div``;
 
@@ -40,6 +41,74 @@ const Price = styled.span`
 	font-size: 40px;
 `;
 
+const FilterContainer = styled.div`
+	width: 50%;
+	margin: 30px 0;
+	display: flex;
+	justify-content: space-between;
+`;
+
+const Filter = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const FilterTitle = styled.span`
+	font-size: 20px;
+	font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+	width: 20px;
+	height: 20px;
+	border-radius: 50%;
+	background-color: ${(props) => props.color};
+	margin: 0px 5px;
+	cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+	margin-left: 10px;
+	padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+	width: 50%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+	display: flex;
+	align-items: center;
+	font-weight: 700;
+`;
+
+const Amount = styled.span`
+	width: 30px;
+	height: 30px;
+	border-radius: 10px;
+	border: 1px solid teal;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin: 0px 5px;
+`;
+
+const Button = styled.button`
+	padding: 15px;
+	border: 2px solid teal;
+	background-color: white;
+	cursor: pointer;
+	font-weight: 500;
+	&:hover {
+		background-color: #f8f4f4;
+	}
+`;
+
 const Product = () => {
 	return (
 		<Container>
@@ -58,6 +127,33 @@ const Product = () => {
 						explicabo dicta odio!
 					</Desc>
 					<Price>80 PLN</Price>
+					<FilterContainer>
+						<Filter>
+							<FilterTitle>Color</FilterTitle>
+							<FilterColor color="black" />
+							<FilterColor color="darkblue" />
+							<FilterColor color="gray" />
+						</Filter>
+						<Filter>
+							<FilterTitle>Size</FilterTitle>
+							<FilterSize>
+								<FilterSizeOption>XS</FilterSizeOption>
+								<FilterSizeOption>S</FilterSizeOption>
+								<FilterSizeOption>M</FilterSizeOption>
+								<FilterSizeOption>L</FilterSizeOption>
+								<FilterSizeOption>XL</FilterSizeOption>
+								<FilterSizeOption>XXL</FilterSizeOption>
+							</FilterSize>
+						</Filter>
+					</FilterContainer>
+					<AddContainer>
+						<AmountContainer>
+							<Remove />
+							<Amount>1</Amount>
+							<Add />
+						</AmountContainer>
+						<Button>ADD TO CART</Button>
+					</AddContainer>
 				</InfoContainer>
 			</Wrapper>
 			<Newsletter />
