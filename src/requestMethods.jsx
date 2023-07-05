54d3f2e12f4e1b48b1027194c119b85da60e14ef
Zasada94 <pawelzasada2@wp.pl>
2023-07-05 14:32:14 +0200

    request methods access token update

--- src/requestMethods.jsx
+++ src/requestMethods.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTE4YmMwOGNkYWVmMzBkNDZmM2M2OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4ODIxMTUxNiwiZXhwIjoxNjg4NDcwNzE2fQ.mIbtNwEx0EUwYXSUnDvjKL0zf3gJ0smq5WFe0q3wjNI";
+
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+	.currentUser.accessToken;
 
 export const publicRequest = axios.create({
 	baseURL: BASE_URL,
