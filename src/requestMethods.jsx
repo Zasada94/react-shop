874634f2b67ea21323bc0ede1b8d41abb5c7680b
Zasada94 <pawelzasada2@wp.pl>
2023-07-07 13:05:49 +0200

    fixed login issue

--- src/requestMethods.jsx
+++ src/requestMethods.jsx
@@ -2,8 +2,15 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-	.currentUser.accessToken;
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+// 	.currentUser.accessToken;
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
+
+// const TOKEN =
+// 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTE4YmMwOGNkYWVmMzBkNDZmM2M2OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4ODU2MDI1MSwiZXhwIjoxNjg4ODE5NDUxfQ.KQVgj_g0GV5mGgbVnCEYVM5M2PVF-2odWAY-b06jCBc";
 
 export const publicRequest = axios.create({
 	baseURL: BASE_URL,
@@ -11,5 +18,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
 	baseURL: BASE_URL,
-	header: { token: `Bearer ${TOKEN}` },
+	headers: { token: `Bearer ${TOKEN}` },
 });
